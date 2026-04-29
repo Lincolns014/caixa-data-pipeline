5a3def4ca9bc004a81f4a4a508cd898bb3dc2d3f
Lincolns <lincolnsud@gmail.com>
2026-04-29 12:46:02 -0300

    refactor: remove arquivos antigos e reorganiza estrutura do projeto

--- main.py
+++
@@ -1,9 +0,0 @@
-from database import criar_tabelas
-from processador_retorno import processar_retorno
-
-def main():
-    criar_tabelas()
-    processar_retorno()
-
-if __name__ == "__main__":
-    main()
